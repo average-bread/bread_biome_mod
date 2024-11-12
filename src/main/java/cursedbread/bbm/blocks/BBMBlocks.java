389f627db4fe55f5166d5dfe6b6880e8137d4c10
average-bread <163648686+average-bread@users.noreply.github.com>
2024-11-12 20:34:30 +0100

    mycelium

--- src/main/java/cursedbread/bbm/blocks/BBMBlocks.java
+++ src/main/java/cursedbread/bbm/blocks/BBMBlocks.java
@@ -1,10 +1,42 @@
 package cursedbread.bbm.blocks;
 
+import net.minecraft.client.render.block.model.BlockModelStandard;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockGlass;
+import net.minecraft.core.block.BlockGrass;
+import net.minecraft.core.block.material.Material;
+import net.minecraft.core.block.tag.BlockTags;
+import net.minecraft.core.sound.BlockSound;
+import turniplabs.halplibe.helper.BlockBuilder;
+
+import static cursedbread.bbm.BBMMain.MOD_ID;
+import static net.minecraft.core.sound.BlockSounds.GRASS;
+
 public class BBMBlocks {
 
 	public static int blockId;
 
-	public void initilizeBlocks(){
+	public static BlockBuilder fullBlock = new BlockBuilder(MOD_ID);
 
+	public static Block mycelium;
+
+	private void initializeBlockDetails(){
+
+	}
+
+	public void initilizeBlocks(){
+		mycelium = fullBlock
+			.setBlockModel(
+				block -> new BlockModelStandard(block)
+					.withTextures("bbm:block/mycelium/mycelium_grass_top", "bbm:block/mycelium/mycelium_dirt", "bbm:block/mycelium/mycelium_grass_side")
+			)
+			.setTags(BlockTags.MINEABLE_BY_SHOVEL, BlockTags.FENCES_CONNECT, BlockTags.FIREFLIES_CAN_SPAWN, BlockTags.GROWS_TREES, BlockTags.CAVES_CUT_THROUGH, BlockTags.CAVE_GEN_REPLACES_SURFACE)
+			.setHardness(0.6f)
+			.setBlockSound(GRASS)
+			.build(new BlockMycelium("mycelium", blockId++, Material.grass));
+
+
+
+		initializeBlockDetails();
 	}
 }
---
+++ src/main/java/cursedbread/bbm/blocks/BlockMycelium.java
@@ -0,0 +1,51 @@
+package cursedbread.bbm.blocks;
+
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockGrass;
+import net.minecraft.core.block.material.Material;
+import net.minecraft.core.data.gamerule.GameRules;
+import net.minecraft.core.world.World;
+import net.minecraft.core.world.biome.Biome;
+import net.minecraft.core.world.biome.Biomes;
+
+import java.util.Random;
+
+public class BlockMycelium extends BlockGrass {
+	public BlockMycelium(String key, int id, Material material) {
+		super(key, id, material);
+		this.setTicking(true);
+	}
+
+	@Override
+	public void updateTick(World world, int x, int y, int z, Random rand) {
+		if (world.isClientSide) {
+			return;
+		}
+		if (world.getBlockId(x, y+1, z) != 0 && world.getBlockId(x, y+1, z) != Block.mushroomBrown.id && world.getBlockId(x, y+1, z) != Block.mushroomRed.id) {
+			if (rand.nextInt(4) != 0) {
+				return;
+			}
+			world.setBlockWithNotify(x, y, z, Block.dirt.id);
+		} else if (world.getBlockLightValue(x, y + 1, z) >= 9) {
+			int l = x + rand.nextInt(3) - 1;
+			int i1 = y + rand.nextInt(5) - 3;
+			int j1 = z + rand.nextInt(3) - 1;
+			int k1 = world.getBlockId(l, i1 + 1, j1);
+			if (world.getBlockId(l, i1, j1) == Block.dirt.id && world.getBlockLightValue(l, i1 + 1, j1) >= 4 && Block.lightBlock[k1] <= 2) {
+				world.setBlockWithNotify(l, i1, j1, this.id);
+			}
+			int idToSpawn = 0;
+			int r = rand.nextInt(400);
+			if (r < 26) {
+				idToSpawn = Block.mushroomRed.id;
+			} else if (r < 41) {
+				idToSpawn = Block.mushroomBrown.id;
+			} else if (r < 60) {
+				idToSpawn = 0;
+			} else {
+				idToSpawn = 0;
+			}
+			world.setBlockWithNotify(x, y + 1, z, idToSpawn);
+		}
+	}
+}
